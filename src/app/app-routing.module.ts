727468503b838c9b202ed8747b2a1dc48a3a760b
Matt <matthew9510@gmail.com>
2020-05-18 19:54:21 -0700

    Setup container for left and right component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ContainerComponent } from "./container/container.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: "container", component: ContainerComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
