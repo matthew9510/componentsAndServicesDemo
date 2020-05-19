c1ba39b7cf92a530e900f406aa0f3db30fd68c00
Matt <matthew9510@gmail.com>
2020-05-18 21:53:47 -0700

    integrated using a service in the left component. Two way data bind our user input to a service rather than a property that lives on the component, so that other components can use the save value.

--- src/app/left-component/left-component.component.ts
+++ src/app/left-component/left-component.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit } from "@angular/core";
-
+import { StorageOneService } from "../services/storage-one.service";
 @Component({
   selector: "app-left-component",
   templateUrl: "./left-component.component.html",
   styleUrls: ["./left-component.component.scss"],
 })
 export class LeftComponentComponent implements OnInit {
-  name: string;
-  constructor() {}
+  // name: string;
+
+  // Don't do this way
+  // serviceReference;
+  // constructor(){
+  //   this.serviceReference = new StorageOneService(cxvcxvcxz);
+  // }
+
+  // constructor(private serviceReference: StorageOneService) {  }
+  constructor(public _storageOneService: StorageOneService) {}
 
   ngOnInit(): void {}
 
   run() {
-    alert(this.name);
+    // alert(this.name)
+    alert(this._storageOneService.name);
   }
 }
