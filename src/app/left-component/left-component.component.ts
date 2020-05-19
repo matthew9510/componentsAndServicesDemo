75bc46411d343e2a0f3fb152bd5061278c3e0d1f
Matt <matthew9510@gmail.com>
2020-05-18 19:19:04 -0700

    created left and right components

---
+++ src/app/left-component/left-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-left-component',
+  templateUrl: './left-component.component.html',
+  styleUrls: ['./left-component.component.scss']
+})
+export class LeftComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
